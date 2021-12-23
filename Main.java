5cf6a34585f1be85a24f3b18b21c73565c5ce217
fatma yamaner <82721046+fatmay@users.noreply.github.com>
2021-12-23 21:53:21 +0000

    fatmay

--- Main.java
+++ Main.java
@@ -1,5 +1,49 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.util.Scanner;
+import java.text.DecimalFormat;
+public class Main {
+    public static void main(String arg [])
+   {
+        Scanner s = new Scanner(System.in);
+         System.out.println("Kilonuzu giriniz: ");
+         double kilo = s.nextFloat();
+         System.out.println("Boyunuzu giriniz (cm cinsinden): ");
+         double boy = s.nextInt();
+         double vki = kilo/Math.pow(boy/100,2);
+         DecimalFormat df = new DecimalFormat("##.##");
+        System.out.println("Vücut kitle indexiniz: "+df.format(vki));
+        System.out.println("Durumunuz: ");
+
+  if(vki<=18.5)
+      {
+          System.out.println("Zayıf");
+         
+
+      }
+
+    else if(vki<=24.9)
+     {
+         System.out.println("Normal");
+
+     }
+
+   else if(vki<=29.9)
+     {
+       System.out.println("Fazla kilolu");
+       
+     }
+
+   else if(vki<=39.9)
+     {
+       System.out.println("Obez");
+        
+
+     }
+
+   else
+    {
+     System.out.println("Aşırı obez");
+     
+
+     }
+   }
 }
